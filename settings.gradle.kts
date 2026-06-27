310080a052a33d483276e2cd7ed00544eed3c53a
Panda17TK <bantian.tech@gmail.com>
2026-06-28 07:36:07 +0900

    build: add Gradle multi-module root and wrapper (8.10.2)

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "arpg"
+include("core", "desktop", "android")
